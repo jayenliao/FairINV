fe6fbaf7120e91318fadadee2d9d89795ac37447
jayenliao <jay.chiehen@gmail.com>
2025-09-26 19:32:52 +0800

    test(commands): for running vanilla GNNs and FairINV

---
+++ commands/run/fairinv_gcn_10runs.sh
@@ -0,0 +1,31 @@
+echo 'Running Vanilla GCN for 10 runs with different random seeds...'
+
+echo
+echo '============German============='
+CUDA_VISIBLE_DEVICES=2 python train_fairinv.py \
+    --model fairinv --encoder gcn --dataset german \
+    --hid_dim 16 --lr 1e-2 --epochs 1000 --seed_num 10 --alpha 10 --lr_sp 0.1
+
+echo
+echo '============Bail============='
+CUDA_VISIBLE_DEVICES=2 python train_fairinv.py \
+    --model fairinv --encoder gcn --dataset bail \
+    --hid_dim 16 --lr 1e-2 --epochs 1000 --seed_num 10 --alpha 10 --lr_sp 0.1
+
+echo
+echo '============Pokec_z============='
+CUDA_VISIBLE_DEVICES=2 python train_fairinv.py \
+    --model fairinv --encoder gcn --dataset pokec_z \
+    --hid_dim 16 --lr 1e-2 --epochs 1000 --seed_num 10 --alpha 10 --lr_sp 0.01
+
+echo
+echo '============Pokec_n============='
+CUDA_VISIBLE_DEVICES=2 python train_fairinv.py \
+    --model fairinv --encoder gcn --dataset pokec_n \
+    --hid_dim 16 --lr 1e-2 --epochs 1000 --seed_num 10 --alpha 1 --lr_sp 0.5
+
+echo
+echo '============nba============='
+CUDA_VISIBLE_DEVICES=2 python train_fairinv.py \
+    --model fairinv --encoder gcn --dataset nba \
+    --hid_dim 16 --lr 1e-2 --epochs 1000 --seed_num 10 --alpha 1 --lr_sp 0.1
