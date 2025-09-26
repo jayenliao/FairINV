d881a9323556fea549de7b621a6f8f9bbcc901ee
jayenliao <jay.chiehen@gmail.com>
2025-09-26 21:05:38 +0800

    refactor(args)

--- train_fairinv.py
+++ train_fairinv.py
@@ -9,49 +9,11 @@ from torch_geometric.loader import DataLoader
 from torch_sparse import SparseTensor
 from tqdm import tqdm
 from datetime import datetime
+from args import get_args
 from load_data import FairDataset
 from utils import Results, set_seed, get_metrics
 from fairinv import FairINV, ConstructModel, fair_metric
 from logger import EpochLogger
-
-def args_parser():
-    parser = argparse.ArgumentParser()
-    parser.add_argument('--model', choices=['fairinv','vanilla'], default='vanilla')
-    parser.add_argument('--no-cuda', action='store_true', default=False,
-                        help='Disables CUDA training.')
-    parser.add_argument('--start_seed', type=int, default=42, help='Random seed start.')
-    parser.add_argument('--seed_num', type=int, default=0, help='The number of random seed.')
-    parser.add_argument('--epochs', type=int, default=1000, help='Number of epochs to train.')
-    parser.add_argument('--lr', type=float, default=0.001, help='Initial learning rate.')
-    parser.add_argument('--weight_decay', type=float, default=1e-5, help='Weight decay (L2 loss on parameters).')
-    parser.add_argument('--hid_dim', type=int, default=16, help='Number of hidden units.')
-    parser.add_argument('--dropout', type=float, default=0.5, help='Dropout rate (1 - keep probability).')
-    parser.add_argument('--dataset', type=str, default='loan',
-                        choices=['nba', 'bail', 'pokec_z', 'pokec_n', 'german'])
-    parser.add_argument("--layer_num", type=int, default=2, help="number of hidden layers")
-    parser.add_argument('--encoder', type=str, default='gcn', choices=['gcn','gat','gin','sage','sgc'])
-    parser.add_argument('--aggr', type=str, default='add',
-                        choices=['add', 'mean', 'max', 'min', 'sum', 'std', 'var', 'median'],
-                        help="aggregation function")
-    parser.add_argument('--weight_path', type=str, default='./weights/model_weight.pt')
-    parser.add_argument('--save_results', type=bool, default=True)
-    parser.add_argument('--alpha', type=float, default=0.5,
-                        help='hyperpapameter to balance the downstream task and invariance learning loss.')
-    parser.add_argument('--lr_sp', type=float, default=0.5, help='the learning rate of the sensitive partition.')
-    parser.add_argument('--env_num', type=int, default=2,
-                        help='the number of the sensitive attribute, also known as environment number.')
-    parser.add_argument('--log_dir', type=str, default='./logs')
-    parser.add_argument('--log_interval', type=int, default=20, help='Interval for logging.')
-    parser.add_argument('--partition_times', type=int, default=3,
-                        help='the number for partitioning the sensitive attribute group.')
-
-    args = parser.parse_known_args()[0]
-    args.cuda = not args.no_cuda and torch.cuda.is_available()
-
-    # set device
-    args.device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
-
-    return args
 
 def set_seed(seed):
     np.random.seed(seed)
@@ -234,7 +196,7 @@ def main(args):
         results.save_results(args)
 
 if __name__ == '__main__':
-    args = args_parser()
+    args = get_args()
     if torch.cuda.is_available():
         torch.multiprocessing.set_start_method('spawn')
     main(args)
