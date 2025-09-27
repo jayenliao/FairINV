61b044c5d8638f3f63019ebb610ec1e2cc9ad1ac
jayenliao <jay.chiehen@gmail.com>
2025-09-27 14:25:23 +0800

    test(commands): for various gnn with different runs

--- commands/run/fairinv_gin_10runs.sh
+++ commands/run/fairinv_gin_10runs.sh
@@ -1,31 +1,31 @@
-echo 'Running Vanilla GAT for 10 runs with different random seeds...'
+echo 'Running Vanilla GIN for 10 runs with different random seeds...'
 
 echo
 echo '============German============='
 CUDA_VISIBLE_DEVICES=2 python train_fairinv.py \
-    --model fairinv --encoder gat --dataset german \
+    --model fairinv --encoder gin --dataset german \
     --hid_dim 16 --lr 1e-2 --epochs 1000 --seed_num 10 --alpha 10 --lr_sp 0.1
 
 echo
 echo '============Bail============='
 CUDA_VISIBLE_DEVICES=2 python train_fairinv.py \
-    --model fairinv --encoder gat --dataset bail \
+    --model fairinv --encoder gin --dataset bail \
     --hid_dim 16 --lr 1e-2 --epochs 1000 --seed_num 10 --alpha 10 --lr_sp 0.1
 
 echo
 echo '============Pokec_z============='
 CUDA_VISIBLE_DEVICES=2 python train_fairinv.py \
-    --model fairinv --encoder gat --dataset pokec_z \
+    --model fairinv --encoder gin --dataset pokec_z \
     --hid_dim 16 --lr 1e-2 --epochs 1000 --seed_num 10 --alpha 10 --lr_sp 0.01
 
 echo
 echo '============Pokec_n============='
 CUDA_VISIBLE_DEVICES=2 python train_fairinv.py \
-    --model fairinv --encoder gat --dataset pokec_n \
+    --model fairinv --encoder gin --dataset pokec_n \
     --hid_dim 16 --lr 1e-2 --epochs 1000 --seed_num 10 --alpha 1 --lr_sp 0.5
 
 echo
 echo '============nba============='
 CUDA_VISIBLE_DEVICES=2 python train_fairinv.py \
-    --model fairinv --encoder gat --dataset nba \
+    --model fairinv --encoder gin --dataset nba \
     --hid_dim 16 --lr 1e-2 --epochs 1000 --seed_num 10 --alpha 1 --lr_sp 0.1
--- commands/run/vanilla_gcn_10runs.sh
+++ commands/run/vanilla_gcn_10runs.sh
@@ -1,31 +1,36 @@
 echo 'Running Vanilla GCN for 10 runs with different random seeds...'
 
 echo
-echo '============German============='xq
+echo '============German============='
 CUDA_VISIBLE_DEVICES=2 python train_fairinv.py \
     --model vanilla --encoder gcn --dataset german \
-    --hid_dim 16 --lr 1e-2 --epochs 1000 --seed_num 10 --alpha 10 --lr_sp 0.1
+    --start_seed 0 --seed_num 10 \
+    --hid_dim 16 --lr 1e-2 --epochs 1000 --alpha 10 --lr_sp 0.1
 
-echo
+echo ''
 echo '============Bail============='
 CUDA_VISIBLE_DEVICES=2 python train_fairinv.py \
     --model vanilla --encoder gcn --dataset bail \
-    --hid_dim 16 --lr 1e-2 --epochs 1000 --seed_num 10 --alpha 10 --lr_sp 0.1
+    --start_seed 0 --seed_num 10 \
+    --hid_dim 16 --lr 1e-2 --epochs 1000 --alpha 10 --lr_sp 0.1
 
-echo
+echo ''
 echo '============Pokec_z============='
 CUDA_VISIBLE_DEVICES=2 python train_fairinv.py \
     --model vanilla --encoder gcn --dataset pokec_z \
-    --hid_dim 16 --lr 1e-2 --epochs 1000 --seed_num 10 --alpha 10 --lr_sp 0.01
+    --start_seed 0 --seed_num 10 \
+    --hid_dim 16 --lr 1e-2 --epochs 1000 --alpha 10 --lr_sp 0.01
 
 echo
 echo '============Pokec_n============='
 CUDA_VISIBLE_DEVICES=2 python train_fairinv.py \
     --model vanilla --encoder gcn --dataset pokec_n \
-    --hid_dim 16 --lr 1e-2 --epochs 1000 --seed_num 10 --alpha 1 --lr_sp 0.5
+    --start_seed 0 --seed_num 10 \
+    --hid_dim 16 --lr 1e-2 --epochs 1000 --alpha 1 --lr_sp 0.5
 
 echo
 echo '============nba============='
 CUDA_VISIBLE_DEVICES=2 python train_fairinv.py \
     --model vanilla --encoder gcn --dataset nba \
-    --hid_dim 16 --lr 1e-2 --epochs 1000 --seed_num 10 --alpha 1 --lr_sp 0.1
+    --start_seed 0 --seed_num 10 \
+    --hid_dim 16 --lr 1e-2 --epochs 1000 --alpha 1 --lr_sp 0.1
