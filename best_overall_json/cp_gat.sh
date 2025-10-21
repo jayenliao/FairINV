98091eaa89128cd954ea4e3164d59cf8a4021887
jayenliao <jay.chiehen@gmail.com>
2025-10-21 22:48:29 +0800

    feat(json)

---
+++ best_overall_json/cp_gat.sh
@@ -0,0 +1,5 @@
+cp ../../../logs_piplup/optuna/bail/gat/vanilla/auc_f1/20251006-151405_auto75/best_overall.json bail.json
+cp ../../../logs_piplup/optuna/pokec_z/gat/vanilla/auc_f1/20251006-210344_auto75/best_overall.json pokec_z.json
+cp ../../../logs_piplup/optuna/pokec_n/gat/vanilla/auc_f1/20251006-164201_auto75/best_overall.json pokec_n.json
+cp ../../../logs_piplup/optuna/nba/gat/vanilla/auc_f1/20251006-165848_auto75/best_overall.json nba.json
+cp ../../../logs_piplup/optuna/german/gat/vanilla/auc_f1/20251006-101506_auto75/best_overall.json german.json
