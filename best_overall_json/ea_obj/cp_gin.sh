9e97503283677c7ac0bb82c85bcd864f7a0e4ff0
jayenliao <jay.chiehen@gmail.com>
2025-11-20 16:30:37 +0800

    fix(best_overall_json): ea_obj - gin - bail

--- best_overall_json/ea_obj/cp_gin.sh
+++ best_overall_json/ea_obj/cp_gin.sh
@@ -3,7 +3,7 @@ datasets=(bail pokec_z pokec_n nba german)
 exp_name=ea_obj
 
 declare -A best_paths=(
-  [bail]="logs/tune_ea/no_attack_obj/bail/gin/edge_adder/auc_f1_balanced/20251118-12055_ea-no-attack/best_overall.json"
+  [bail]="logs/tune_ea/no_attack_obj/bail/gin/edge_adder/auc_f1_balanced/20251118-103816_ea-no-attack/best_overall.json"
   [pokec_z]="logs/tune_ea/no_attack_obj/pokec_z/gin/edge_adder/auc_f1_balanced/20251118-140057_ea-no-attack/best_overall.json"
   [pokec_n]="logs/tune_ea/no_attack_obj/pokec_n/gin/edge_adder/auc_f1_balanced/20251118-193606_ea-no-attack/best_overall.json"
   [nba]="logs/tune_ea/no_attack_obj/nba/gin/edge_adder/auc_f1_balanced/20251119-015636_ea-no-attack/best_overall.json"
