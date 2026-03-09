d2a2993fe0eb0e37e0376067cc532a2536187629
jayenliao <jay.chiehen@gmail.com>
2026-03-09 09:47:23 +0800

    fix(commands/adv/): correct the exp assignment

--- commands/adv/at_fair-only_gat_1.sh
+++ commands/adv/at_fair-only_gat_1.sh
@@ -1,12 +1,13 @@
-GPU_ID=5 EPOCHS=200 SEED_NUM=10 ADV_K=5 ADV_EDGE_POLICY="global_random" \
+
+GPU_ID=6 EPOCHS=200 SEED_NUM=10 ADV_K=5 ADV_EDGE_POLICY="same_largest" \
     LAMBDA_DP=2.0 LAMBDA_EO=2.0 ADV_EDGE_STEPS=4 \
-    DATASETS="bail pokec_z pokec_n" ENCODERS="gcn" \
+    DATASETS="bail pokec_z pokec_n" ENCODERS="gat" \
     ADVTRAIN_ATTACK=edge_weight \
     bash commands/adv/advtrain_edge_clean_and_attacked.sh
 
-GPU_ID=5 EPOCHS=200 SEED_NUM=10 ADV_K=5 ADV_EDGE_POLICY="same_random" \
+GPU_ID=6 EPOCHS=200 SEED_NUM=10 ADV_K=5 ADV_EDGE_POLICY="same_smallest" \
     LAMBDA_DP=2.0 LAMBDA_EO=2.0 ADV_EDGE_STEPS=4 \
-    DATASETS="bail pokec_z pokec_n" ENCODERS="gcn" \
+    DATASETS="bail pokec_z pokec_n" ENCODERS="gat" \
     ADVTRAIN_ATTACK=edge_weight \
     bash commands/adv/advtrain_edge_clean_and_attacked.sh
 
